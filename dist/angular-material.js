bf71bb7c531c6e539e67a7d0268cd235397dd087
EC2 Default User <ec2-user@ip-172-31-58-60.ec2.internal>
2015-10-28 05:10:48 +0000

    rebuild

--- dist/angular-material.js
+++ dist/angular-material.js
@@ -17843,8 +17843,7 @@ angular
  */
 
 function MdAutocomplete () {
-  var hasNotFoundTemplate = false;
-
+  
   return {
     controller:   'MdAutocompleteCtrl',
     controllerAs: '$mdAutocompleteCtrl',
@@ -17870,7 +17869,7 @@ function MdAutocomplete () {
       inputId:        '@?mdInputId'
     },
     link: function(scope, element, attrs, controller) {
-      controller.hasNotFound = hasNotFoundTemplate;
+      controller.hasNotFound = !!attrs.hasNotFoundTemplate;
     },
     template:     function (element, attr) {
       var noItemsTemplate = getNoItemsTemplate(),
@@ -17879,7 +17878,7 @@ function MdAutocomplete () {
           tabindex        = attr.tabindex;
 
       if (noItemsTemplate) {
-        hasNotFoundTemplate = true;
+        attr.hasNotFoundTemplate = true;
       }
 
       if (attr.hasOwnProperty('tabindex')) {
